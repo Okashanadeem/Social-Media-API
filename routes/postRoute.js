913ace15688110cc3999c3461e410873a89da2ed
Okashanadeem <okashanadeem0101@gmail.com>
2025-09-04 17:17:16 +0500

    final push

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -21,6 +21,8 @@ const { postValidator } = require('../validators/postValidator')
 const { auth } = require('../middlewares/authMiddleware')
 const upload = require('../middlewares/upload')
 
+const requireRole = require('../middlewares/requireRole')
+
 // POST ROUTES
 router.post('/', auth, upload.single("image"), validate(postValidator), createPost)
 router.get('/', auth, getAllPosts)
@@ -28,7 +30,7 @@ router.get('/comments', auth, getAllComments)
 router.get('/users/:username', auth, getPostByUsername)
 router.get('/:id', auth, getPostById)
 router.put('/:id', auth, updatePostById)
-router.delete('/:id', auth, deletePostById)
+router.delete('/:id', auth, requireRole('admin','user'), deletePostById)
 
 // LIKE ROUTE
 router.post('/:id/like', auth, toggleLike)
@@ -36,7 +38,7 @@ router.post('/:id/like', auth, toggleLike)
 // COMMENTS ROUTES
 router.post('/:id/comments', auth, createComments)
 router.get('/:id/comments', auth, getComments)
-router.put('/:id/comments/:commentId', auth, updateCommentById)  
-router.delete('/:id/comments/:commentId', auth, deleteCommentById) 
+router.put('/:id/comments/:commentId', auth, requireRole('admin','user'), updateCommentById)  
+router.delete('/:id/comments/:commentId', auth, requireRole('admin','user'), deleteCommentById) 
 
 module.exports = router
